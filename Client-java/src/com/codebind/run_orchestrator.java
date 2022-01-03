f88710edffae8910011d10660c093ee0622f9236
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-01-03 18:17:53 +0000

    Generate ID button now works

---
+++ Client-java/src/com/codebind/run_orchestrator.java
@@ -0,0 +1,13 @@
+package com.codebind;
+
+import java.io.IOException;
+import org.json.JSONObject;
+
+public class run_orchestrator {
+    public static String main() throws IOException {
+        String response = test.get_response.main("http://localhost:8080/api/orchestrator?service=ID");
+        JSONObject json_message = new JSONObject(response);
+        return json_message.getString("id");
+    }
+
+}
--- Client-java/src/com/codebind/test.java
+++ Client-java/src/com/codebind/test.java
@@ -3,9 +3,14 @@ package com.codebind;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class test {
-    private JButton clickHereButton;
+    private JButton proposalButton;
     private JPanel panelMain;
     private JTextField id_field;
     private JLabel SCCTripsLabel;
@@ -16,10 +21,29 @@ public class test {
 
 
     public test() {
-        clickHereButton.addActionListener(new ActionListener() {
+        proposalButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("hello world! U just pressed my buttons");
+            }
+        });
+
+        queryProposalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        generateIDButton.addActionListener(new ActionListener()  {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                try {
+                    String id = run_orchestrator.main();
+                    id_field.setText(id);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
     }
@@ -29,5 +53,24 @@ public class test {
         frame.setContentPane(new test().panelMain);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static class get_response {
+        public static String main(String url_str) throws IOException {
+            URL url = new URL(url_str);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
+
+            BufferedReader reader;
+            String line;
+            StringBuilder responseContent = new StringBuilder();
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+            con.disconnect();
+            return responseContent.toString();
+        }
     }
 }
